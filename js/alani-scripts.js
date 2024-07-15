eb4b15892d316a00f707f8ea8307111eaa0ee97a
gtjones97 <gabriel897@me.com>
2024-07-15 18:13:12 -0400

    debug form 5 remove more validation

--- js/alani-scripts.js
+++ js/alani-scripts.js
@@ -111,60 +111,6 @@ $(function () {
 		}
 	}, 'a.btn, .btn');
 
-	/* Validate contact form */
-	$("#cform").validate({
-		rules: {
-			name: {
-				required: true
-			},
-			message: {
-				required: true
-			},
-			email: {
-				required: true,
-				email: true
-			}
-		},
-		success: "valid",
-		submitHandler: function() {
-			$.ajax({
-				url: 'mailer/feedback.php',
-				type: 'post',
-				dataType: 'json',
-				data: 'name='+ $("#cform").find('input[name="name"]').val() + '&email='+ $("#cform").find('input[name="email"]').val() + '&message=' + $("#cform").find('textarea[name="message"]').val(),
-				beforeSend: function() {
-
-				},
-				complete: function() {
-
-				},
-				success: function(data) {
-					$('#cform').fadeOut();
-					$('.alert-success').delay(1000).fadeIn();
-				}
-			});
-		}
-	});
-
-	/* Validate commect form */
-	$("#comment_form").validate({
-		rules: {
-			name: {
-				required: true
-			},
-			message: {
-				required: true
-			},
-			email: {
-				required: true,
-				email: true
-			}
-		},
-		success: "valid",
-		submitHandler: function() {
-		}
-	});
-
 	/* Initialize masonry items */
 	var $container_clients = $('.section.clients .box-items');
 	$container_clients.imagesLoaded(function() {
